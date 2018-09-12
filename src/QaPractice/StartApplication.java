78a3dfe79badbc381581dc2f1e3bba2866b8f655
ShubhiSD <shubhi.shukla.07@gmail.com>
2018-09-13 01:49:12 +0530

    test script added

---
+++ src/QaPractice/StartApplication.java
@@ -0,0 +1,71 @@
+package QaPractice;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.nativekey.AndroidKey;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import com.sun.glass.events.KeyEvent;
+
+public class StartApplication {
+
+		private AndroidDriver driver;
+		public static void main(String[] args) throws MalformedURLException, InterruptedException {
+
+			File classpathRoot = new File(System.getProperty("user.dir"));
+			File appDir = new File(classpathRoot, "/Apps/Amazon/");
+			File app = new File(appDir, "in.amazon.mShop.android.shopping.apk");
+
+			DesiredCapabilities capabilities = new DesiredCapabilities();
+			capabilities.setCapability("deviceName", "Mi Phone");
+			capabilities.setCapability("platformVersion", "7.1.1");
+			capabilities.setCapability("platformName", "Android");
+			capabilities.setCapability("app", app.getAbsolutePath());
+			capabilities.setCapability("noSign","True"); 
+			capabilities.setCapability("appPackage", "com.amazon.mShop.android.shopping");
+			capabilities.setCapability("appActivity", "com.amazon.mShop.home.HomeActivity");
+			StartApplication appvar=new StartApplication();
+			appvar.driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+			appvar.driver.manage().timeouts().implicitlyWait(80, TimeUnit.SECONDS);
+			Thread.sleep(10000);
+			appvar.signIn();
+			appvar.searchItem();
+			appvar.selectFirstItemFromSearch();
+			appvar.closeConnection();
+
+	}
+		private void signIn() {
+			driver.findElementById("com.amazon.mShop.android.shopping:id/sign_in_button").click();
+			driver.findElementById("login_accordion_header").click();
+			driver.findElementById("ap_email_login").sendKeys("shubhi.shukla.07@gmail.com\n");
+			driver.findElementById("ap_password").sendKeys("aashi@143\n");
+			if(driver.findElementById("com.amazon.mShop.android.shopping:id/action_bar_home_logo").isDisplayed())
+			System.out.println("sign in successful");
+			}
+		private void searchItem()
+		{
+		
+			WebElement searchBox=driver.findElementById("com.amazon.mShop.android.shopping:id/rs_search_src_text");
+			searchBox.sendKeys("iPhone9\n");
+			//driver.pressKeyCode(AndroidKeyCode.KEYCODE_NUMPAD_ENTER );
+			//driver.pressKey(new KeyEvent(AndroidKey.ENTER));
+			//driver.getKeyboard().sendKeys(Keys.RETURN);
+		}
+		private void selectFirstItemFromSearch() {
+			List<WebElement> item=driver.findElementsByXPath("\\*[contains(text(),'Apple iPhone')]");
+			item.get(1).click();
+			//add to cart
+			driver.findElementById("add-to-cart-button").click();
+		}
+		private void closeConnection()
+		{driver.quit();}
+
+}
---
+++ src/test/AppiumTest.java
@@ -0,0 +1,36 @@
+package test;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+ 
+import org.openqa.selenium.remote.DesiredCapabilities;
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+ 
+public class AppiumTest {
+	
+	public static void main(String[] args){
+		
+		//Set the Desired Capabilities
+		DesiredCapabilities caps = new DesiredCapabilities();
+		caps.setCapability("deviceName", "Mi Phone");
+		caps.setCapability("udid", "62f4c258"); //Give Device ID of your mobile phone
+		caps.setCapability("platformName", "Android");
+		caps.setCapability("platformVersion", "7.1.1");
+		caps.setCapability("appPackage", "com.android.vending");
+		caps.setCapability("appActivity", "com.android.vending.AssetBrowserActivity");
+		caps.setCapability("noReset", "true");
+		
+		//Instantiate Appium Driver
+		try {
+				AppiumDriver<MobileElement> driver = new AndroidDriver<MobileElement>(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+			
+		} catch (MalformedURLException e) {
+			System.out.println(e.getMessage());
+			
+			
+		}
+	}
+ 
+}
